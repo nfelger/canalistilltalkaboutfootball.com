afbd2beac00f4bdc782b3173c45d59d429affa67
Niko Felger <niko.felger@gmail.com>
2012-10-13 17:07:51 +0100

    Caching goal count in redis; rending goals to the page straight away.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'nokogiri'
+gem 'redis-rb
--- web.rb
+++ web.rb
@@ -2,13 +2,19 @@ require 'sinatra'
 require 'open-uri'
 require 'nokogiri'
 
-get '/' do
-  erb :'index.html'
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
-get '/torres_score.json' do
+get '/' do
+  erb :'index.html', :locals => {:goals => REDIS.get("torres_goals")}
+end
+
+get '/refresh_score' do
   html = open('http://news.bbc.co.uk/sport1/hi/football/eng_prem/top_scorers/default.stm') { |file| file.read}
   doc = Nokogiri::HTML(html)
   torres_goals = doc.css('table.fulltable tr:contains("Torres")').first.text.split.last
-  "{score: #{torres_goals}}"
+  REDIS.set("torres_goals", torres_goals)
 end
