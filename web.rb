a0bd2dfdc453116f9b1b8d5cd483e64861ca7577
Niko Felger <niko.felger@gmail.com>
2012-10-13 15:18:00 +0100

    Pulling Torres' score from the BBC

--- web.rb
+++ web.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'open-uri'
+require 'nokogiri'
 
 get '/' do
   erb :'index.html'
+end
+
+get '/torres_score.json' do
+  html = open('http://news.bbc.co.uk/sport1/hi/football/eng_prem/top_scorers/default.stm') { |file| file.read}
+  doc = Nokogiri::HTML(html)
+  torres_goals = doc.css('table.fulltable tr:contains("Torres")').first.text.split.last
+  "{score: #{torres_goals}}"
 end
